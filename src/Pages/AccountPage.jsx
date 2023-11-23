d6092168dc1c9aa6b3e52c21a064aa3b88d36631
dvirak <dvirak43@vt.edu>
2023-11-23 13:17:45 -0500

    Available Books Feature

--- src/Pages/AccountPage.jsx
+++ src/Pages/AccountPage.jsx
@@ -1,13 +1,9 @@
 import Login from "../components/Login";
 import Register from "../components/Register";
 import Account from "../components/Account";
-import { useEffect, useState } from "react";
 
 export default function AccountPage() {
-  const [userKey, setUserKey] = useState(() =>
-    localStorage.getItem("current-user-key")
-  );
-
+  const userKey = localStorage.getItem("current-user-key");
   return (
     <div className="account-section">
       {userKey ? (
